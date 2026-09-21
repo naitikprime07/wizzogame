8dec510d41e09ee75218d006ecf0a6d3d180a816
naitikprime07 <naitikprime07@gmail.com>
2026-09-21 14:52:44 +0530

    MIME type file excute error fix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
 
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: fileURLToPath(new URL('./index.html', import.meta.url)),
+                game: fileURLToPath(new URL('./game.html', import.meta.url)),
+                gamedet: fileURLToPath(new URL('./gamedet.html', import.meta.url)),
+                classify: fileURLToPath(new URL('./classify.html', import.meta.url)),
+                search: fileURLToPath(new URL('./search.html', import.meta.url)),
+                privacy: fileURLToPath(new URL('./Privacy.html', import.meta.url)),
+                terms: fileURLToPath(new URL('./Terms.html', import.meta.url)),
+                copyright: fileURLToPath(new URL('./copyright.html', import.meta.url))
+            }
+        }
+    }
+});
